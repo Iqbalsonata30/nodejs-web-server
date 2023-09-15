674ac6efb0434497c609ce04a0492ea0ea2f989d
Iqbal Sonata <iqbalsonata2@gmail.com>
2023-09-15 09:28:23 +0700

    Created POST API

--- server.js
+++ server.js
@@ -8,17 +8,18 @@ const requestListener = (req,res) => {
     if (method == "GET"){
         res.end("<h1>Hello GET</h1>")
     }
-    if (method == "POST"){
-       res.end("<h1>Hello POST</h1>")
-    }
-    if (method == "PUT"){
-        res.end("<h1>Hello PUT</h1>")
-    }
-    if (method == "DELETE"){
-        res.end("<h1>Hello DELETE</h1>")
-    }
+    if (method === "POST"){
+        let body = []
+        req.on('data', (chunk) => {
+            body.push(chunk)
+        })
 
-
+        req.on('end',() => {
+            body = Buffer.concat(body).toString();
+            const { name } = JSON.parse(body)
+            res.end(`<h1>Hai,${name}</h1>`)
+        })
+    }
 }
 
 const server = http.createServer(requestListener)
